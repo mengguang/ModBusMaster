94606fde04557b9909556da3a5b8a385bc4fed36
Meng Guang <mengguang@newtonproject.org>
2019-04-27 15:42:58 +0800

    extract modbus_read_device function.

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <modbus.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+
+int modbus_read_device(modbus_t *ctx, int device_address, int register_address, int number_of_register, uint16_t *result) {
+    if (modbus_connect(ctx) == -1) {
+        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+        modbus_free(ctx);
+        return -1;
+    }
+    int rc;
+    int i;
+    modbus_set_slave(ctx, device_address);
+    rc = modbus_read_registers(ctx, register_address, number_of_register, result);
+    if (rc == -1) {
+        fprintf(stderr, "%s\n", modbus_strerror(errno));
+        return -1;
+    }
+    for (i = 0; i < rc; i++) {
+        printf("reg[%d]=%d (0x%04X)\n", i, result[i], result[i]);
+    }
+    modbus_close(ctx);
+    return rc;
+}
 
 int main() {
     modbus_t *ctx;
@@ -14,41 +38,24 @@ int main() {
     }
 
     modbus_set_debug(ctx, TRUE);
-    modbus_set_slave(ctx, 1);
-
-//    if (modbus_rtu_set_serial_mode(ctx,MODBUS_RTU_RS485) == -1) {
-//        fprintf(stderr, "modbus_rtu_set_serial_mode failed: %s\n", modbus_strerror(errno));
-//        modbus_free(ctx);
-//        return -1;
-//    }
-
-    if (modbus_connect(ctx) == -1) {
-        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
-        modbus_free(ctx);
-        return -1;
-    }
 
     uint32_t old_response_to_sec;
     uint32_t old_response_to_usec;
 
     /* Save original timeout */
     modbus_get_response_timeout(ctx, &old_response_to_sec, &old_response_to_usec);
-    printf("old_response_to_sec: %u\nold_response_to_usec: %u\n", old_response_to_sec, old_response_to_usec);
+    printf("old_response_to_sec: %u\nold_response_to_usec: %u\n",
+            old_response_to_sec, old_response_to_usec);
 
     /* Define a new timeout of 200ms */
     modbus_set_response_timeout(ctx, 0, 200000);
 
-    rc = modbus_read_registers(ctx, 0, 3, tab_reg);
-    if (rc == -1) {
-        fprintf(stderr, "%s\n", modbus_strerror(errno));
-        return -1;
-    }
+    //temperature, humidity, co2
+    modbus_read_device(ctx,1,0,3,tab_reg);
 
-    for (i = 0; i < rc; i++) {
-        printf("reg[%d]=%d (0x%04X)\n", i, tab_reg[i], tab_reg[i]);
-    }
+    //0-200000Lux light sensor
+    modbus_read_device(ctx,2,2,2,tab_reg);
 
-    modbus_close(ctx);
     modbus_free(ctx);
     return 0;
 }
